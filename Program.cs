bf71e5de876bc0d983320a8865b1e38aa9e2d244
Juliana <jdiaspurcino@gmail.com>
2024-08-12 13:19:05 -0300

    feat: created file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+List<BigInt> sort(BigInt[] array)
+{
+    
+}
+
+
+public class BigInt
+{
+    public static BigInt operator +(BigInt a, BigInt b) { }
+
+    public static bool operator >(BigInt a, BigInt b) { }
+
+    public static bool operator <(BigInt a, BigInt b) { }
+
+    public static bool operator ==(BigInt a, BigInt b) { }
+
+    public static bool operator !=(BigInt a, BigInt b) { }
+
+}
